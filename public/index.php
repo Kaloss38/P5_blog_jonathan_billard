d9d3c038b731451313fa57da48ad0063461574a2
jonathan-b-dev <jonathan.billard@halias.fr>
2021-04-16 19:04:26 +0200

    Update architecture files

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require "../vendor/autoload";
+define("CONF_DIR", realpath(dirname(__DIR__)) . "/config" );
+
+define("VIEW_DIR", realpath(dirname(__DIR__)) . "/src/Views" );
+
+//VIEW_DIR . "home.view.php";
---
+++ src/Core/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core;
+
+class Controller
+{
+    //init twig render, redirect
+    //flash message
+}
